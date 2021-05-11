61e13c0c2132e2d3419e7a6b7bf926f30372ed32
92hisa <73869235+92hisa@users.noreply.github.com>
2021-05-11 12:08:26 +0900

    chenged mailer error from true to false on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'https://blooming-everglades-83728.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors=false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
