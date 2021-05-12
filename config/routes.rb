18597a64d3f69a5f7f23d67290e233d863502576
92hisa <73869235+92hisa@users.noreply.github.com>
2021-05-12 18:20:16 +0900

    edited sitemap.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { registrations: 'registrations' }
   root 'tops#index'
-  get 'sitemap', to: redirect('https://keiken.s3-ap-northeast-1.amazonaws.com/sitemap.xml.gz')
+  get 'sitemap', to: redirect('https://s3-ap-northeast-1.amazonaws.com/keiken/sitemaps/sitemap.xml.gz')
   get :dynamic_select_category, to: 'posts#dynamic_select_category'
   get '/search', to: 'posts#search', as: :search
   get '/search_header', to: 'posts#search_header', as: :search_header
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,8 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.keiken.info"
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.public_path = 'public/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
   ENV['S3_BUCKET_NAME'],
   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
