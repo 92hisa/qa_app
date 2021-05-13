1bf001fac389e3a4267f2b165c4ede169fb30642
92hisa <73869235+92hisa@users.noreply.github.com>
2021-05-13 15:00:02 +0900

    added method to redirect from heroku domain to keiken domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :ensure_domain
+  FQDN = 'www.keiken.info'
+
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+
+    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
 
   protected
 
