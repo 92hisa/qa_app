d93d973aabb6e1a795f7e567efa4de7e22921a61
92hisa <73869235+92hisa@users.noreply.github.com>
2021-05-13 15:14:26 +0900

    edited ensure_domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :ensure_domain
+  before_action :ensure_domain
   FQDN = 'www.keiken.info'
 
   def ensure_domain
