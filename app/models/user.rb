13bfe9ef2b9cfa60495ddc1a85c9120d9fbc9782
92hisa <73869235+92hisa@users.noreply.github.com>
2021-05-15 22:40:44 +0900

    added dependent destroy as validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :likes
-  has_many :answers
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :answers, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
