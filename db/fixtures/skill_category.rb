47181fc79382b94ba2b19c9dafadb3500740e9ae
92hisa <73869235+92hisa@users.noreply.github.com>
2021-05-20 20:17:59 +0900

    cleaned public

---
+++ db/fixtures/skill_category.rb
@@ -0,0 +1,16 @@
+# Category.seed(:id,
+# # parent
+# {:id=>1001, :name=>"ブログ", :ancestry=>nil},
+# {:id=>1002, :name=>"webマーケティング", :ancestry=>nil},
+# {:id=>1003, :name=>"プログラミング", :ancestry=>nil},
+# # children(ブログ)
+# {:id=>10011, :name=>"プログ開設", :ancestry=>1001},
+# {:id=>10012, :name=>"集客", :ancestry=>1001},
+# {:id=>10013, :name=>"SEO対策", :ancestry=>1001},
+# {:id=>10013, :name=>"アフィリエイト", :ancestry=>1001},
+# {:id=>10013, :name=>"ツール", :ancestry=>1001},
+# # children(webマーケティング)
+# {:id=>10021, :name=>"広告運用全般", :ancestry=>1002},
+# {:id=>10022, :name=>"", :ancestry=>1002},
+# {:id=>10023, :name=>"LINE", :ancestry=>1002},
+# {:id=>10023, :name=>"Facebook", :ancestry=>1002},
